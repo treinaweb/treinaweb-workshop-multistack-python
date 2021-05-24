b444b9e83e7b4f98e702fd482037e4365a2527d1
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-05-24 09:12:18 -0300

    removendo exclusao

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('cadastrar_diarista', cadastrar_diarista, name='cadastrar_diarista'),
     path('listar_diaristas', listar_diaristas, name='listar_diaristas'),
     path('editar_diarista/<int:diarista_id>', editar_diarista, name='editar_diarista'),
-    path('remover_diarista/<int:diarista_id>', remover_diarista, name='remover_diarista')
+    # path('remover_diarista/<int:diarista_id>', remover_diarista, name='remover_diarista')
 ]
